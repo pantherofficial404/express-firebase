839f3786ea7389b34b565aa201b6cd15871affdc
pantherofficial404 <pantherofficial404@gmail.com>
2019-09-05 21:47:09 +0530

    Auth Functionality Added

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-import * as firebase from 'firebase';
+const firebase = require("firebase/app");
+require("firebase/storage");
+require("firebase/database");
+require('firebase/auth');
 
 class firebaseServices {
 
@@ -19,6 +22,7 @@ class firebaseServices {
     firebase.initializeApp(firebaseConfig);
     this.firestore = firebase.database();
     this.firebaseStorage = firebase.storage();
+    this.firebaseAuth = firebase.auth();
   }
 
   async post(path, data) {
@@ -43,7 +47,6 @@ class firebaseServices {
     const dbRef = await this.firestore.ref(apiPath);
     await dbRef.remove();
   }
-
   async uploadFile(fileName, file) {
     const fileRef = await this.firebaseStorage.ref().child(fileName).put(file)
       .then(snapshot => {
@@ -53,6 +56,244 @@ class firebaseServices {
         return downloadURL;
       });
     return fileRef;
+  }
+
+  async signup(email, password) {
+    return this.firebaseAuth.createUserWithEmailAndPassword(email, password)
+      .then((data) => {
+        return {
+          code: 200,
+          data: {
+            email: email,
+            id: data.user.uid,
+            refreshToken: data.user.refreshToken,
+            isVerified: data.user.emailVerified,
+          },
+          success: true,
+        }
+      })
+      .catch((err) => {
+        if (err.code === 'auth/email-already-in-use') {
+          return {
+            code: 400,
+            data: {
+              message: 'User is already exists',
+            },
+            success: false,
+          }
+        }
+        if (err.code === 'auth/weak-password') {
+          return {
+            code: 400,
+            data: {
+              message: 'Password is too weak',
+            },
+            success: false,
+          }
+        }
+        return {
+          code: 400,
+          data: {
+            message: 'Something went wrong.'
+          },
+          success: false,
+        }
+      });
+  };
+
+  async login(email, password) {
+    return this.firebaseAuth.signInWithEmailAndPassword(email, password)
+      .then((data) => {
+        console.log(data);
+        return {
+          code: 200,
+          data: {
+            email: email,
+            id: data.user.uid,
+            isVerified: data.user.emailVerified,
+            refreshToken: data.user.refreshToken,
+          },
+          success: true,
+        }
+      })
+      .catch((err) => {
+        if (err.code === 'auth/user-not-found') {
+          return {
+            code: 400,
+            data: {
+              message: 'User not found',
+            },
+            success: false,
+          }
+        }
+        if (err.code === 'auth/wrong-password') {
+          return {
+            code: 400,
+            data: {
+              message: 'Incorrect password',
+            },
+            success: false,
+          }
+        }
+        return {
+          code: 400,
+          data: {
+            message: 'Something went wrong',
+          },
+          success: false,
+        }
+      });
+  }
+
+
+  async logout() {
+    return this.firebaseAuth.signOut()
+      .then((data) => {
+        return {
+          code: 200,
+          data: {
+            message: 'Logout successfull',
+          },
+          success: true,
+        }
+      })
+      .catch((err) => {
+        return {
+          code: 400,
+          data: {
+            message: 'Something went wrong',
+          },
+          success: false,
+        }
+      });
+  }
+
+  async getCurrentUser() {
+    const user = this.firebaseAuth.currentUser;
+    if (user) {
+      return {
+        code: 200,
+        data: {
+          email: user.email,
+          id: user.uid,
+          refreshToken: user.refreshToken,
+          isVerified: data.user.emailVerified,
+        },
+        success: true,
+      }
+    }
+    return {
+      code: 404,
+      data: {
+        message: 'User is not logged in',
+      },
+      success: false,
+    }
+  }
+
+  async isLoggedIn() {
+    const user = this.firebaseAuth.currentUser;
+    if (user) {
+      return {
+        code: 200,
+        data: {
+          message: true,
+        },
+        success: true,
+      }
+    }
+    return {
+      code: 404,
+      data: {
+        message: false,
+      },
+      success: false,
+    }
+  }
+
+  async sendEmailVerification() {
+    const user = this.firebaseAuth.currentUser;
+    if (user) {
+      user.sendEmailVerification();
+    } else {
+      return {
+        code: 404,
+        data: {
+          message: 'User is not logged in',
+        },
+        success: false,
+      }
+    }
+  }
+
+  async loginWithGoogle() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    return this.firebaseAuth.signInWithPopup(provider)
+      .then((data) => {
+        return {
+          code: 200,
+          data: {
+            email: data.user.email,
+            id: data.user.uid,
+            refreshToken: data.user.refreshToken,
+            isVerified: data.user.emailVerified,
+          },
+          success: true,
+        }
+      })
+      .catch((err) => {
+        if (err.code === 'auth/account-exists-with-different-credential') {
+          return {
+            code: 400,
+            data: {
+              message: 'Account is already exists with this email',
+            },
+            success: false,
+          }
+        }
+        return {
+          code: 400,
+          data: {
+            message: 'Unable to login',
+          },
+          success: false,
+        }
+      });
+  }
+
+  async loginWithGithub() {
+    const provider = new firebase.auth.GithubAuthProvider();
+    return this.firebaseAuth.signInWithPopup(provider)
+      .then((data) => {
+        return {
+          code: 200,
+          data: {
+            email: data.user.email,
+            id: data.user.uid,
+            refreshToken: data.user.refreshToken,
+            isVerified: data.user.emailVerified,
+          },
+          success: true,
+        }
+      })
+      .catch((err) => {
+        if (err.code === 'auth/account-exists-with-different-credential') {
+          return {
+            code: 400,
+            data: {
+              message: 'Account is already exists with this email',
+            },
+            success: false,
+          }
+        }
+        return {
+          code: 400,
+          data: {
+            message: 'Unable to login',
+          },
+          success: false,
+        }
+      });
   }
 }
 export default new firebaseServices();
